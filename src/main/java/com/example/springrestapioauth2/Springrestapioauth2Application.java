961efea6ab3ed9746091e9bf4b3daed979ac7674
inafsdeveloper <inafsdeveloper@gmail.com>
2024-07-13 15:23:25 -0700

    Initial Commit

---
+++ src/main/java/com/example/springrestapioauth2/Springrestapioauth2Application.java
@@ -0,0 +1,13 @@
+package com.example.springrestapioauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springrestapioauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springrestapioauth2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springrestapioauth2/Springrestapioauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springrestapioauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springrestapioauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
